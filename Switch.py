2e6e00a95c64e7f785ca3622882d8f807f330799
Brendan Griffin <brendg@microsoft.com>
2020-05-12 18:03:14 +0100

    Commit

---
+++ Switch.py
@@ -0,0 +1,17 @@
+#Use a dictionary to perform a switch
+def monthswitch(value):
+    months = {
+        1: "January",
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December"
+        }
+    print(months.get(value, "Invalid Month"))
