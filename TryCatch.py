4498fc7177070f56c662d28765118c55c6c0373d
Brendan Griffin <brendankgriffin@hotmail.com>
2021-01-05 17:43:43 +0000

    Commit

--- TryCatch.py
+++ TryCatch.py
@@ -28,7 +28,6 @@ if num < 6:
 
 #Integrate into Try, Except - if the entered number is less than 6 generate an exception
 number = input("Enter Number...")
-
 try:
    if int(number) < 6:
        raise Exception("Number must be greater than 5") 
