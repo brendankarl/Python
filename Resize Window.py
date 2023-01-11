0fdbfa9b0b3cdfa4513ca729eeb04f75610c1960
Brendan Griffin <brendankgriffin@hotmail.com>
2023-01-11 12:37:46 +0000

    Update Resize Window.py

--- Resize Window.py
+++ Resize Window.py
@@ -1,10 +1,15 @@
 # pip install PyGetWindow
 import pygetwindow
 
+# Get all of the currently opened windows
 windows = pygetwindow.getAllTitles()
 
+# Print a list of the currently opened windows
 for window in windows:
     print(window)
 
-edge = pygetwindow.getWindowsWithTitle("Untitled - Notepad")[0]
-edge.resizeTo(1920, 1080)
+# Specify the name of the window to resize
+notepad = pygetwindow.getWindowsWithTitle("Untitled - Notepad")[0]
+
+#resize the window
+notepad.resizeTo(1920, 1080)
